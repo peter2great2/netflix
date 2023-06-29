9443859d6d4ad96a1276c8311f9fae33f3bb1b9c
peter2great2 <peter2great2@gmail.com>
2023-06-29 17:19:54 +0100

    all files added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Row from "./components/Row";
+
+function App() {
+  return (
+    <div className="App">
+      <Row title="NETFLIX ORIGINAL" />
+      <Row title="Trending Now" />
+      <Row title="New" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/request.js
@@ -0,0 +1,6 @@
+const API_KEY = "69e00b68d84bed6323001210c2e54598";
+
+const request = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `discover/tv?api_key=${API_KEY}&with_networks=213`,
+};
